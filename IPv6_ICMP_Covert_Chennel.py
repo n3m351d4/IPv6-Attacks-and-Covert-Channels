da33e046d36d666ea601559f95b9379f42a9ae8a
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-16 14:06:51 +0300

    Create IPv6_ICMP_Covert_Chennel.py

---
+++ IPv6_ICMP_Covert_Chennel.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+from scapy.layers.inet import UDP
+from scapy.layers.inet6 import IPv6, ICMPv6ND_RA, ICMPv6EchoRequest
+
+number_packets = 1000
+
+
+def icmp_covert_cnannel():
+    print("ICMP Covert Channel")
+    destination = "fe80::a00:27ff:fe4c:1052"
+    source = "fe80::a00:27ff:fe3b:c7d"
+    l3 = IPv6(dst=destination, src=source)
+    h = ICMPv6EchoRequest()
+    l4 = UDP()
+    payload = Raw(load="7495 N3m351d4 Inside")
+    packets = l3 / h / l4 / payload
+    packets.show()
+    send(packets, count=int(number_packets))
+
+
+icmp_covert_cnannel()
+
+
