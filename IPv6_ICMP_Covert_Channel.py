036c928fdc9071c87309a9994d81f2bf1ea49c51
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-16 15:45:25 +0300

    Update IPv6_ICMP_Covert_Channel.py

--- IPv6_ICMP_Covert_Channel.py
+++ IPv6_ICMP_Covert_Channel.py
@@ -2,7 +2,7 @@
 
 from scapy.all import *
 from scapy.layers.inet import UDP
-from scapy.layers.inet6 import IPv6, ICMPv6ND_RA, ICMPv6EchoRequest
+from scapy.layers.inet6 import IPv6, ICMPv6EchoRequest
 
 number_packets = 1000
 
@@ -12,9 +12,9 @@ def icmp_covert_cnannel():
     destination = "fe80::a00:27ff:fe4c:1052"
     source = "fe80::a00:27ff:fe3b:c7d"
     l3 = IPv6(dst=destination, src=source)
-    h = ICMPv6EchoRequest()
+    h = ICMPv6EchoRequest(data="lol")
     l4 = UDP()
-    payload = Raw(load="7495 N3m351d4 Inside")
+    payload = Raw(load="DC7495 N3m351d4 Inside")
     packets = l3 / h / l4 / payload
     packets.show()
     send(packets, count=int(number_packets))
