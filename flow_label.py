82c4c5794fbc0c90713d417ff0657bc9148dd0bf
N3m351d4 <42093594+n3m351d4@users.noreply.github.com>
2020-04-16 14:22:41 +0300

    Update flow_label.py

--- flow_label.py
+++ flow_label.py
@@ -13,15 +13,16 @@ def flow_label():
     destination = "fe80::a00:27ff:fe4c:1052"
     source = "fe80::a00:27ff:fe3b:c7d"
     payload_fl = int(binascii.hexlify(b"DC"), 16)
-    l3 = IPv6(dst=destination, src=source, fl=payload_fl)  #
-    # l3 = IPv6(dst=destination, src=source, fl=RandNum(1, 1048575))
-
+    l3 = IPv6(dst=destination, src=source, fl=payload_fl)
     l4 = UDP()
-    # payload = Raw(load=RandString(10))
-    payload = Raw(load="7495 N3m351d4 Inside")
+    payload = Raw(load=RandString(10))
     packets = l3 / l4 / payload
     packets.show()
     send(packets, count=int(number_packets))
 
 
 flow_label()
+
+
+
+
